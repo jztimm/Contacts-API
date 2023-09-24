4b6c6b11e6143c30bfdfdc62c87459be618b07c4
jztimm <jztimm@gmail.com>
2023-09-24 17:25:07 -0400

    Create and update Routes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Route
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
