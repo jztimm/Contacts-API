21b4396632bfd079e80f89362e1b77a1665021dd
jztimm <jztimm@gmail.com>
2023-09-24 17:51:02 -0400

    Update Contact Controller for CRUD opperations and used built in middleware

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-// Route
+// Middleware
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
